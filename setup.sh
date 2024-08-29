f4c89d7d34fe7c920ecaf7a7e319d37c044cf318
DreamHacker112 <mohdsaif78428@gmail.com>
2024-08-29 06:02:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,70 +4,45 @@
 update_repository() {
     echo "Updating repository..."
     bash update.sh
-    if [ $? -eq 0 ]; then
-        echo "Repository updated successfully!"
-    else
-        echo "Failed to update repository."
-        exit 1
-    fi
+    echo "Repository updated successfully!"
 }
 
 # Main script
 clear
 echo
+
+# Install pv and fix permissions if necessary
 pkg install pv -y >/dev/null 2>&1
+chmod 755 /data/data/com.termux/files/usr/bin/pv
 
-echo -e "\033[32m\033[1m{───────────────────────────────────────────────}"
+echo -e "\033[32m\033[1m{─────────────────────────────────────────────}"
 echo -e "\033[33m\033[1m Installing All Required Packages! Please Wait..." | pv -qL 10
-
-apt update -y
-apt upgrade -y
-pkg install python -y
-pkg install cmatrix -y
-pkg install pv -y
-apt install figlet -y
-apt install ruby -y
-apt install mpv -y
-pip install lolcat
-pip install requests
-pkg install python2 -y
-pkg install termux-api -y
-
+apt update -y                 
+apt upgrade -y 
+pkg install python -y 
+pkg install cmatrix -y 
+pkg install pv -y 
+apt install figlet -y  
+apt install ruby -y 
+apt install mpv -y 
+pip install lolcat 
+pip install random 
+pip install requests 
+pkg install python2 -y 
+pkg install termux-api -y 
 echo -e "\033[31m\033[1m      👿 INSTALLATION COMPLETED 👿 \033[32m[\033[36m✓\033[32m]" | pv -qL 12
 echo -e "\033[33m\033[1m]──────────────────────────────────────────────["
 
 termux-setup-storage
-
-# Create necessary directories if they do not exist
-mkdir -p $HOME/TermuX-Custom/Song
+cd $HOME/TermuX-Custom
+cp login.sh $PREFIX/etc
+chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh update.sh
+mkdir -p $HOME/TermuX-Custom/Song 
+mv Access-Granted.mp3 Jarvis2.mp3 JARVIS.mp3 sound_effect.py $HOME/TermuX-Custom/Song
+rm 1
 mkdir -p $HOME/TermuX-Custom/NETWORK
-
-# Check and move files
-if [ -f "login.sh" ]; then
-    cp login.sh $PREFIX/etc
-    chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh update.sh
-else
-    echo "login.sh not found."
-    exit 1
-fi
-
-if [ -f "Access-Granted.mp3" ] && [ -f "Jarvis2.mp3" ] && [ -f "JARVIS.mp3" ] && [ -f "sound_effect.py" ]; then
-    mv Access-Granted.mp3 Jarvis2.mp3 JARVIS.mp3 sound_effect.py $HOME/TermuX-Custom/Song
-else
-    echo "One or more files not found for Song directory."
-    exit 1
-fi
-
-if [ -f "network.py" ]; then
-    mv network.py $HOME/TermuX-Custom/NETWORK
-else
-    echo "network.py not found."
-    exit 1
-fi
+mv network.py $HOME/TermuX-Custom/NETWORK
 
 # Prompt user for update
 # Set default choice to 'y', update the repository, and run the login script
-choice="y"
-update_repository
-bash login.sh
-Enter
+choice="y" && update_repository && bash login.sh
